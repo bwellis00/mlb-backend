f4b4424e583fdef5db6bd9bddbe8d4b3f75f0819
unknown <brandonellis@gmail.com>
2019-09-24 16:18:01 -0500

    git push heroku master

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
@@ -15,4 +17,4 @@ const statsRouter = require('./routes/stats')
 app.use('/stats', statsRouter)
 
 
-app.listen(3000, () => console.log('server started'))
+app.listen(PORT, () => console.log('server started'))
